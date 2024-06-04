c2e21410011475fb3958b7e875d5294f5408e0ca
unknown <arafathyasir4000@gmail.com>
2024-06-04 17:30:23 +0530

    first commit

--- src/Calculator2/Calculator2.js
+++ src/Calculator2/Calculator2.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../Calculator2/Calci.css";
 
 const Calculator2 = () => {
   const [currentOp, setCurrentOp] = useState("");
@@ -52,64 +53,151 @@ const Calculator2 = () => {
     setOutput("");
   };
 
-  // array of button val n operators
-  const digits1 = ["1", "2", "3", "4", "5"];
-  const digits2 = ["6", "7", "8", "9", "0"];
+  const digits1 = ["7", "4", "1", "0"];
+  const digits2 = ["8", "5", "2"];
+  const digits3 = ["9", "6", "3"];
   const operators = ["+", "-", "*", "/"];
 
   return (
-    <div className="container d-flex align-center justify-center">
-      <div className="container ">
-        <div>
+    <div className="d-flex justify-content-center align-items-center vh-100 calculator-out">
+      <div className="container-md calculator-in">
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+              <div className="col-12">
+                <input
+                  type="text"
+                  value={`${currentInput} `}
+                  className="border border-info mb-2 w-100 input"
+                  readOnly
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3 p-1">
+                {digits1.map((digit) => (
+                  <input
+                    key={digit}
+                    type="button"
+                    value={digit}
+                    onClick={() => addInput(digit)}
+                    className="btn btn-primary  btn-lg mb-1 w-14 h-14 w-100 shadow-lg button"
+                  />
+                ))}
+              </div>
+              <div className="col-3 p-1">
+                {digits2.map((digit) => (
+                  <input
+                    key={digit}
+                    type="button"
+                    value={digit}
+                    onClick={() => addInput(digit)}
+                    className="btn btn-primary btn-lg mb-1 w-14 h-14 w-100 shadow-lg button"
+                  />
+                ))}
+                <input
+                  type="button"
+                  value="="
+                  onClick={calculate}
+                  className="btn btn-primary  btn-lg mb-1  w-14 h-14 w-100 shadow-lg button"
+                />
+              </div>
+              <div className="col-3 p-1">
+                {digits3.map((digit) => (
+                  <input
+                    key={digit}
+                    type="button"
+                    value={digit}
+                    onClick={() => addInput(digit)}
+                    className="btn btn-primary btn-lg mb-1 w-14 h-14 w-100 shadow-lg button"
+                  />
+                ))}
+                <input
+                  type="button"
+                  value="AC"
+                  onClick={reset}
+                  className="btn btn-primary btn-lg mb-1 h-14 w-100 shadow-lg button"
+                />
+              </div>
+              <div className="col-3 p-1">
+                {operators.map((operator) => (
+                  <input
+                    key={operator}
+                    type="button"
+                    value={operator}
+                    onClick={() => op(operator)}
+                    className="btn btn-primary btn-lg mb-1 w-14 h-14 w-100 shadow-lg  button"
+                  />
+                ))}
+                <input
+                  type="button"
+                  value="Del"
+                  className="btn btn-primary btn-lg mb-1 w-14 h-14 w-100 shadow-lg  button"
+                  onClick={handleDelete}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 output">Output: {output}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator2;
+
+/* 
+
+<div className="calculator-container">
+        <input
+          type="text"
+          value={`${currentInput} `}
+          className="border border-info m-1"
+          readOnly
+        />
+        <div className="d-flex flex-wrap justify-content-center">
+          {digits1.map((digit) => (
+            <input
+              key={digit}
+              type="button"
+              value={digit}
+              onClick={() => addInput(digit)}
+              className="btn btn-primary m-1 w-14 h-14 "
+            />
+          ))}
+        </div>
+        <div className="d-flex flex-wrap justify-content-center">
+          {digits2.map((digit) => (
+            <input
+              key={digit}
+              type="button"
+              value={digit}
+              onClick={() => addInput(digit)}
+              className="btn btn-primary m-1 w-14 h-14"
+            />
+          ))}
+        </div>
+        <div className="d-flex justify-content-center">
+          {operators.map((operator) => (
+            <input
+              key={operator}
+              type="button"
+              value={operator}
+              onClick={() => op(operator)}
+              className="btn btn-primary m-1 w-14 h-14"
+            />
+          ))}
           <input
-            type="text"
-            value={`${currentInput} `}
-            className="border border-info m-1 "
-            readOnly
+            type="button"
+            value="Del"
+            className="btn btn-primary m-1 w-14 h-14"
+            onClick={handleDelete}
           />
         </div>
-        <div>
-          <div className="">
-            {digits1.map((digit) => (
-              <input
-                key={digit}
-                type="button"
-                value={digit}
-                onClick={() => addInput(digit)}
-                className="btn btn-primary m-1 w-14 h-14"
-              />
-            ))}
-          </div>
-          <div>
-            {digits2.map((digit) => (
-              <input
-                key={digit}
-                type="button"
-                value={digit}
-                onClick={() => addInput(digit)}
-                className="btn btn-primary m-1 w-14 h-14"
-              />
-            ))}
-          </div>
-          <div>
-            {operators.map((operator) => (
-              <input
-                key={operator}
-                type="button"
-                value={operator}
-                onClick={() => op(operator)}
-                className="btn btn-primary m-1 w-14 h-14"
-              />
-            ))}
-            <input
-              type="button"
-              value="Del"
-              className="btn btn-primary m-1 w-14 h-14"
-              onClick={handleDelete}
-            />
-          </div>
-        </div>
-        <div>
+        <div className="d-flex justify-content-center">
           <input
             type="button"
             value="="
@@ -123,12 +211,9 @@ const Calculator2 = () => {
             className="btn btn-primary m-1 h-14"
           />
         </div>
-        <div>
+        <div className="d-flex justify-content-center">
           <p>Output: {output}</p>
         </div>
       </div>
-    </div>
-  );
-};
 
-export default Calculator2;
+*/
